854b7b06ed01db06ff5649c5f2f3bc1b98923769
dmonicag <monicagandhi89@gmail.com>
2023-02-26 16:49:27 +0530

    Exc 4.6 mostBlogs test function

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -1,27 +1,38 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
-    if(Array.isArray(blogs))
-        return 1
+  if(Array.isArray(blogs))
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    const listofLikes = blogs.map(blog => blog.likes)
-    let sum = 0;
-    for (let i = 0; i < listofLikes.length; i++) 
+  const listofLikes = blogs.map(blog => blog.likes)
+  let sum = 0;
+  for (let i = 0; i < listofLikes.length; i++) 
     {
-        sum += listofLikes[i];
+      sum += listofLikes[i];
     }
-    return sum
+  return sum
 }
 
 const favouriteBlog = (blogs) => {
-    const maxLikes = Math.max(...blogs.map(m => m.likes))
-    const fav = blogs.find(b => b.likes === maxLikes)
-   // console.log(fav)
-    return fav
+  const maxLikes = Math.max(...blogs.map(m => m.likes))
+  const fav = blogs.find(b => b.likes === maxLikes)
+  return fav
+}
+
+const mostBlogs = (blogs) => {
+  var authorArray =  _.map(blogs, 'author')
+  var mostBlogs_author = _.head(_(authorArray).countBy().entries().maxBy(_.last))
+  const a = blogs.filter(b => b.author === mostBlogs_author)
+  var author_blogs = { "author": mostBlogs_author, "blogs": a.length}
+
+  return author_blogs
 }
 
 module.exports = {
-    dummy,
-    totalLikes,
-    favouriteBlog
-  }
+  dummy,
+  totalLikes,
+  favouriteBlog,
+  mostBlogs
+}
