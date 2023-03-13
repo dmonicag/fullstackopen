e7a0b1bd531a1a45811074ffe02d53937299a971
dmonicag <monicagandhi89@gmail.com>
2023-03-13 20:43:10 +0530

    Exc 1.7

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -7,11 +7,16 @@ const Button = (props) => {
 }
 
 const Display = (props) => {
+  const average = ((props.good*1) + (props.neutral*0) + (props.bad*(-1))) / props.total.length
+  const positivePercentage = ((props.good) / props.total.length) * 100
   return(
     <div>
       <p>Good &nbsp; {props.good}</p>
       <p>Neutral &nbsp; {props.neutral}</p>
       <p>Bad &nbsp; {props.bad}</p>
+      <p>Total &nbsp; {props.good + props.neutral + props.bad}</p>
+      <p>Average &nbsp; {average}</p>
+      <p>Positive &nbsp; {positivePercentage}%</p>
     </div>
   )
 }
@@ -20,17 +25,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState([])
 
   const increaseGood = () => {
     setGood(good + 1)
+    setTotal(total.concat('G'))
   }
 
   const increaseNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total.concat('N'))
   }
 
   const increaseBad = () => {
     setBad(bad + 1)
+    setTotal(total.concat('B'))
   }
 
   return (
@@ -40,7 +49,7 @@ const App = () => {
       <Button handleClick={increaseNeutral} text='Neutral'/>&nbsp;
       <Button handleClick={increaseBad} text='Bad'/>
       <h2>Statistics</h2>
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} total={total} />
     </div>
   )
 }
