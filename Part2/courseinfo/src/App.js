a900d3810da57980b0c2df0b5734bfac056cf92b
dmonicag <monicagandhi89@gmail.com>
2023-03-15 12:38:03 +0530

    Exc 2.5

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -1,46 +1,4 @@
-const Course = (props) => {
-  return(
-    <div>
-      <Content course={props.course} />
-    </div>
-  )
-}
-const Content = (props) => {
-  return(
-   <div>
-    {props.course.map(c => 
-      <div key={c.id}>
-       <Header course={c}/>
-        {
-         c.parts.map(p =>
-          <p key={p.id}>
-           {p.name}&nbsp;&nbsp;{p.exercises}           
-          </p>)
-        }
-        <Total parts={c.parts}/>
-      </div>)}
-   </div>
-  )
-}
-const Header = (props) => {
-  return(
-    <div>
-      <h2>
-      {props.course.name}
-      </h2>
-    </div>
-  )
-}
-const Total = (props) => {
-  const exercises= props.parts.map(total => total.exercises)
-  //using reduce
-  const total = exercises.reduce((a, b) => a + b, 0)
-  return (
-    <div>
-      <p><b>Total of {total} exercises</b></p>
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const course = [
