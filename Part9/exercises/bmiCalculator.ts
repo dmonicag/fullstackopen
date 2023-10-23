f072d877163de21f2eb59e098b1eeadd6ce30f77
dmonicag <monicagandhi89@gmail.com>
2023-10-23 17:23:51 +0530

    error handling for bmiCalculator

--- Part9/exercises/bmiCalculator.ts
+++ Part9/exercises/bmiCalculator.ts
@@ -3,21 +3,43 @@ type Result = 'Underweight' | 'Normal' | 'Overweight' | 'Obese';
 const weight: number = Number(process.argv[2])
 const height: number = Number(process.argv[3])
 
-const calculateBmi = (weight: number, height: number): Result => {
-    const heightInMeters = height/100
-    const bmi = weight / (heightInMeters ** 2)
-        
-    if(bmi <= 18.5){
-      return 'Underweight'
-    } else if (bmi > 18.5 && bmi <= 24.9){
-      return 'Normal'
-    } else if (bmi > 25 && bmi <=29.9){
-      return 'Overweight'
-    } else if (bmi >= 30){
-      return 'Obese'
-    }   
+const calculateBmi = (args: string[]): Result => {
+  if(args.length < 4){
+    throw new Error("Please add arguments:")
   }
 
-console.log(calculateBmi(weight, height));
+  if(args.length > 4){
+    throw new Error("too many arguments")
+  }
+
+  if(isNaN(Number(args[2])) || isNaN(Number(args[3]))){
+    throw new Error("inputs are not numbers")
+  }
+
+  const heightInMeters = Number(args[2])/100
+  const bmi = Number(args[3]) / (heightInMeters ** 2)
+        
+  if(bmi <= 18.5){
+    return 'Underweight'
+  } else if (bmi > 18.5 && bmi <= 24.9){
+    return 'Normal'
+  } else if (bmi > 25 && bmi <=29.9){
+    return 'Overweight'
+  } else 
+    return 'Obese'
+}
+
+  try{
+    calculateBmi(process.argv);
+  }
+  catch(error: unknown){
+    let errorMessage = "Error:"
+    if (error instanceof Error){      
+      errorMessage += error.message;
+    }
+    console.log(errorMessage)
+  }
+
+console.log(calculateBmi(process.argv));
   
   
